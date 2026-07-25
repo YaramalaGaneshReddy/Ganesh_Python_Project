581f26f92cd08a3c5cbff81a17b34891985383f7
GANESH REDDY YARAMALA <ganeshreddy.yaramala126909@marwadiuniversity.ac.in>
2026-07-25 08:45:15 +0530

    Update requirements.txt and fix encoding

--- run.ps1
+++ run.ps1
@@ -1,3 +1,4 @@
 Write-Host "Starting Ganesh Store E-Commerce Application..." -ForegroundColor Green
+Set-Location $PSScriptRoot
 Start-Process "http://127.0.0.1:8000/"
-& ".\venv\Scripts\python.exe" manage.py runserver 8000
+& "$PSScriptRoot\venv\Scripts\python.exe" "$PSScriptRoot\manage.py" runserver 8000
